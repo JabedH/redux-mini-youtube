d722cdab8f3325711ccd400d666831683be5351c
jabedH <jabedhtusar@gmail>
2022-11-08 21:40:14 +0600

    added new item

---
+++ src/features/paginationFilter/pagination.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const pagination = () => {
+    return (
+        <div>
+            <h1>this is pagination</h1>
+        </div>
+    );
+};
+
+export default pagination;
